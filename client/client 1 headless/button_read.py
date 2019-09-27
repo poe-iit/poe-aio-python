96ff284cf3885b8895f96a38e03ffaa5222814ad
Travis Smith <travissmith1998@gmail.com>
2019-09-27 13:42:10 -0500

    fix button code

--- client/client 1 headless/button_read.py
+++ client/client 1 headless/button_read.py
@@ -2,23 +2,30 @@
 
 import RPi.GPIO as gpio
 
-gpio.setmode(gpio.BCM)
-gpio.setup(18, gpio.OUT)
+#gpio.setmode(gpio.BCM)
+#gpio.setup(18, gpio.OUT)
 
 # add rising edge detection on a channel
-GPIO.add_event_detect(17, GPIO.RISING) # For fire
-GPIO.add_event_detect(27, GPIO.RISING) # For shooter
-
+#GPIO.add_event_detect(17, GPIO.RISING) # For fire
+#GPIO.add_event_detect(27, GPIO.RISING) # For shooter
 
 class Button():
 
     def listen_for_press():
         while True:
-            if GPIO.event_detected(17):
+            if GPIO.input(20):
                 print('Button 1 pressed')
-                return 1
-            if GPIO.event_detected(27):
+            if GPIO.event_detected(21):
                 print('Button 2 pressed')
                 # return and send shooter message to server
-                return 2
             time.sleep(0.0001)
+
+
+def main ():
+    Button.listen_for_press()
+    GPIO.setupmode(GPIO.BCM)
+    GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(21, GPIO.IN)
+
+if __name__ == '__main__':
+    main()
