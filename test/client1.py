93618d670ee9651729ba7e75c030ddaff8a88808
Travis Smith <travissmith1998@gmail.com>
2019-11-15 13:56:35 -0600

    fix

--- test/client1.py
+++ test/client1.py
@@ -1,26 +1,13 @@
-import socket
-import select
-import sys
+#client use decode
+from socket import gethostname, socket
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-if len(sys.argv) != 3:
-    print("Correct usage: script, IP address, port number")
-    exit()
-IP_address = str(sys.argv[1])
-Port = int(sys.argv[2])
-server.connect((IP_address, Port))
+serSocket = socket()
+server = "192.168.2.50"
+port = 12345
+serSocket.connect((server, port))
 
-while True:
-    sockets_list = [sys.stdin, server]
-    read_sockets,write_socket, error_socket = select.select(sockets_list, [], [])
-    for socks in read_sockets:
-        if socks == server:
-            message = socks.recv(2048)
-            print (message)
-        else:
-            message = sys.stdin.readline()
-            server.send(message)
-            sys.stdout.write("<You>")
-            sys.stdout.write(message)
-            sys.stdout.flush()
-server.close()
+data = serSocket.recv(1024)
+msg = data.decode('utf-8')
+print("Returned Msg from server:  <{}>".format(msg))
+
+serSocket.close()
